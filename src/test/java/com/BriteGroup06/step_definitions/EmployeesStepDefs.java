70f29551120abcf6d84ed9fc8eaf777ecc968bcf
letmetest1822-sami <letmetest1822@gmail.com>
2020-06-17 07:29:48 +0300

    Commit from sami BasePage, EmployeesPage, EmployeesStepDefs, EmployeesFeature at 17062020 morning

---
+++ src/test/java/com/BriteGroup06/step_definitions/EmployeesStepDefs.java
@@ -0,0 +1,28 @@
+package com.BriteGroup06.step_definitions;
+
+import com.BriteGroup06.pages.EmployeesPage;
+import com.BriteGroup06.pages.LoginPage;
+import com.BriteGroup06.utilities.BrowserUtils;
+import com.BriteGroup06.utilities.ConfigurationReader;
+import com.BriteGroup06.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class EmployeesStepDefs {
+
+    EmployeesPage employeesPage =  new EmployeesPage();
+    @Given("the user is logged in with proper credentials")
+    public void the_user_is_logged_in_with_proper_credentials() {
+        new LoginPage().login(ConfigurationReader.get("username"), ConfigurationReader.get("password"));
+    }
+
+    @Then("page should have the title {string}")
+    public void page_should_have_the_title(String string) {
+        BrowserUtils.waitFor(1);
+        employeesPage.goToEmployeesTab();
+        BrowserUtils.waitFor(2);
+        System.out.println("Driver.get().getTitle() = " + Driver.get().getTitle());
+    }
+
+
+}
