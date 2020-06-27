88df63eb2002d929a177c85279ad5a394f62067e
hiltas73 <h_iltas@yahoo.com>
2020-06-27 10:50:28 +0300

    update

--- src/test/java/com/BriteGroup06/step_definitions/ContactsStepDefs.java
+++ src/test/java/com/BriteGroup06/step_definitions/ContactsStepDefs.java
@@ -22,7 +22,4 @@ public class ContactsStepDefs {
         new ContactsPage().createNewContact("Osman Osman","Ataturk Mh 1938 sk no:23","Selanik");
         Assert.assertTrue("Verify Contact is created", new ContactsPage().contactCreatedMsg.isDisplayed());
     }
-
-
-
 }
