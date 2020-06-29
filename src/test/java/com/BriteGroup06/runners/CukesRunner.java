8b7e421f7d26adbb06bc4c09e9665524ab9f35ed
hiltas73 <h_iltas@yahoo.com>
2020-06-29 19:13:40 +0300

    29.06 19.15

--- src/test/java/com/BriteGroup06/runners/CukesRunner.java
+++ src/test/java/com/BriteGroup06/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features/",
         glue = "com/BriteGroup06/step_definitions",
         dryRun =false,
-        tags = "@halim"
+        tags = "@wip"
 
         )
 public class CukesRunner {
